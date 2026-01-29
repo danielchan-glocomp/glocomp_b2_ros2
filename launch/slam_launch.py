911528137f1cd6163b80dc06920051f78bf90bbd
danielchan-glocomp <danielchan@glocomp.net>
2026-01-29 11:17:30 +0800

    Update slam_launch.py

--- launch/slam_launch.py
+++ launch/slam_launch.py
@@ -38,7 +38,7 @@ def generate_launch_description():
     lifecycle_nodes = ['map_saver']
 
     # Getting directories and launch-files
-    bringup_dir = get_package_share_directory('glocomp_b2_ws')
+    bringup_dir = get_package_share_directory('glocomp_b2_ros2')
     slam_toolbox_dir = get_package_share_directory('slam_toolbox')
     slam_launch_file = os.path.join(slam_toolbox_dir, 'launch', 'online_sync_launch.py')
 
