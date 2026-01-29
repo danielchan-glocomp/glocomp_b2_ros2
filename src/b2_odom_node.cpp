3532d45fce52470b65d07fedc3393c23fdf24673
Dan <interwin.daniel@gmail.com>
2026-01-29 10:53:41 +0800

    Added some comments for better readability

--- src/b2_odom_node.cpp
+++ src/b2_odom_node.cpp
@@ -19,14 +19,16 @@ public:
   OdomPublisher()
   : Node("b2_odom_publisher")
   {
-    // Initialize the transform broadcaster
+    // Initialize the odom publisher
     odom_pub_ = this->create_publisher<nav_msgs::msg::Odometry>(
         "/odom",10);
+
+    // Lol
     time_sub = this->create_subscription<nav_msgs::msg::Odometry>(
 	"dog_odom", 1,
 	std::bind(&OdomPublisher::get_time, this, std::placeholders::_1));
 
-
+    // Grab the odometry
     std::string topic_name = "sportmodestate";
     subscription_ = this->create_subscription<unitree_go::msg::SportModeState>(
       topic_name, 10,
@@ -42,27 +44,19 @@ private:
   void handle_odom(const std::shared_ptr<unitree_go::msg::SportModeState> msg)
   {
     nav_msgs::msg::Odometry odom;
-
-    // Read message content and assign it to
-    // corresponding tf variables
     odom.header.stamp = stamp;
 
     odom.header.frame_id = "odom";
     odom.child_frame_id = "robot_center";
 
-
-    // Turtle only exists in 2D, thus we get x and y translation
-    // coordinates from the message and set the z coordinate to 0
     odom.pose.pose.position.x = msg->position[0];
     odom.pose.pose.position.y = msg->position[1];
     odom.pose.pose.position.z = msg->position[2];
-
 
     odom.pose.pose.orientation.x = msg->imu_state.quaternion[1];
     odom.pose.pose.orientation.y = msg->imu_state.quaternion[2];
     odom.pose.pose.orientation.z = msg->imu_state.quaternion[3];
     odom.pose.pose.orientation.w = msg->imu_state.quaternion[0];
-
 
     odom.twist.twist.linear.x = msg->velocity[0];
     odom.twist.twist.linear.y = msg->velocity[1];
--- src/b2_tf_node.cpp
+++ src/b2_tf_node.cpp
@@ -22,11 +22,13 @@ public:
     // Initialize the transform broadcaster
     tf_broadcaster_ =
       std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
+    // I will not explain why you need to do this unless I get a salary increment
     time_sub = this->create_subscription<nav_msgs::msg::Odometry>(
 	"dog_odom", 1,
 	std::bind(&FramePublisher::get_time, this, std::placeholders::_1));
 
-
+    // Getting the Odometry for the robot
     std::string topic_name = "sportmodestate";
     subscription_ = this->create_subscription<unitree_go::msg::SportModeState>(
       topic_name, 10,
@@ -41,69 +43,29 @@ private:
 
   void handle_tf(const std::shared_ptr<unitree_go::msg::SportModeState> msg)
   {
+    // Create the odom -> "Base link" transform, will not elaborate
     geometry_msgs::msg::TransformStamped t;
-
-    // Read message content and assign it to
-    // corresponding tf variables
     t.header.stamp = stamp;
-
     t.header.frame_id = "odom";
     t.child_frame_id = "robot_center";
-
-
-    // Turtle only exists in 2D, thus we get x and y translation
-    // coordinates from the message and set the z coordinate to 0
     t.transform.translation.x = msg->position[0];
     t.transform.translation.y = msg->position[1];
     t.transform.translation.z = msg->position[2];
-
-    // For the same reason, turtle can only rotate around one axis
-    // and this why we set rotation in x and y to 0 and obtain
-    // rotation in z axis from the message
-    //tf2::Quaternion q;
-    //q.setRPY(M_PI, 0, 0);
     t.transform.rotation.x = msg->imu_state.quaternion[1];
     t.transform.rotation.y = msg->imu_state.quaternion[2];
     t.transform.rotation.z = msg->imu_state.quaternion[3];
     t.transform.rotation.w = msg->imu_state.quaternion[0];
-    //tf2::Quaternion q_orig(
-    //    t.transform.rotation.x,
-    //    t.transform.rotation.y,
-    //    t.transform.rotation.z,
-    //    t.transform.rotation.w
-    //);
-    // Multiply (tf2 ONLY)
-    //tf2::Quaternion q_new = q * q_orig;
-    //q_new.normalize();
-
-    // Convert back to geometry_msgs
-    //t.transform.rotation.x = q_new.x();
-    //t.transform.rotation.y = q_new.y();
-    //t.transform.rotation.z = q_new.z();
-    //t.transform.rotation.w = q_new.w();
-
-
     // Send the transformation
     tf_broadcaster_->sendTransform(t);
 
+    // Salary increment?
     geometry_msgs::msg::TransformStamped t1;
-
-    // Read message content and assign it to
-    // corresponding tf variables
     t1.header.stamp = stamp;
     t1.header.frame_id = "robot_center";
     t1.child_frame_id = "rslidar";
-
-
-    // Turtle only exists in 2D, thus we get x and y translation
-    // coordinates from the message and set the z coordinate to 0
     t1.transform.translation.x = 0;
     t1.transform.translation.y = 0;
     t1.transform.translation.z = 0;
-
-    // For the same reason, turtle can only rotate around one axis
-    // and this why we set rotation in x and y to 0 and obtain
-    // rotation in z axis from the message
     tf2::Quaternion q1;
     q1.setRPY(0, 0, 0);
     t1.transform.rotation.x = q1.x();
@@ -113,24 +75,14 @@ private:
     // Send the transformation
     tf_broadcaster_->sendTransform(t1);
 
+    // Lol
     geometry_msgs::msg::TransformStamped t2;
-    // Read message content and assign it to
-    // corresponding tf variables
     t2.header.stamp = stamp;
-
     t2.header.frame_id = "robot_center";
     t2.child_frame_id = "base_footprint";
-
-
-    // Turtle only exists in 2D, thus we get x and y translation
-    // coordinates from the message and set the z coordinate to 0
     t2.transform.translation.x = 0;
     t2.transform.translation.y = 0;
     t2.transform.translation.z = -t.transform.translation.z;
-
-    // For the same reason, turtle can only rotate around one axis
-    // and this why we set rotation in x and y to 0 and obtain
-    // rotation in z axis from the message
     tf2::Quaternion q2;
     q2.setRPY(0, 0, 0);
     t2.transform.rotation.x = q2.x();
