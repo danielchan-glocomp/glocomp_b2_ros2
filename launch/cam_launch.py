7f40b46a792cb422192ca3ab4cc4c7eb28552cca
danielchan-glocomp <danielchan@glocomp.net>
2026-01-29 11:12:41 +0800

    Update cam_launch.py

--- launch/cam_launch.py
+++ launch/cam_launch.py
@@ -6,11 +6,11 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():    
-    share_dir = get_package_share_directory('glocomp_b2_ws')
+    share_dir = get_package_share_directory('glocomp_b2_ros2')
     launch_dir = os.path.join(share_dir, 'launch')
     
     front_cam = Node(
-        package='glocomp_b2_ws',
+        package='glocomp_b2_ros2',
         executable='front_cam',
         name='front_cam',
         output='screen',
@@ -18,25 +18,14 @@ def generate_launch_description():
     )
 
     back_cam = Node(
-        package='glocomp_b2_ws',
+        package='glocomp_b2_ros2',
         executable='back_cam',
         name='back_cam',
         output='screen',
         parameters=[]
     )
-
-    cmd_vel = Node(
-        package='glocomp_b2_ws',
-        executable='cmd_vel_subscriber',
-        name='cmd_vel',
-        output='screen',
-        parameters=[]
-    )
-
-
     
     ld = LaunchDescription()
     ld.add_action(front_cam)
     ld.add_action(back_cam)
-    ld.add_action(cmd_vel)
     return ld
